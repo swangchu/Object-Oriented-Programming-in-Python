46e2c26d5a29a82b86188f2b70bdee0b36ae599c
Sonam Wangchu <swangchu79@gmail.com>
2023-01-17 09:20:18 +0600

    Create Activity-1.py

---
+++ Activity-1.py
@@ -0,0 +1,30 @@
+#Write a program to simulate bank transactions for two clients in a procedural paradigm. 
+#The clients can deposit, withdraw and check their balance.
+
+balance = 0
+
+def deposit(amount):
+    global balance
+    balance += amount
+
+def withdraw(amount):
+    global balance
+    if balance >= amount:
+        balance -= amount
+    else:
+        print("Insufficient funds.")
+def check_balance():
+    global balance
+    print("Your current balance is:", balance)
+
+
+
+#First Client transaction 
+deposit(100)
+withdraw(50)
+print(“The bank balance for First client is ”,check_balance()) #must print "Your current balance is: 50"
+
+#Second Client Transaction
+deposit(200)
+withdraw(150)
+print(“The bank balance for Second client is ”,check_balance())  #must print "Your current balance is: 50"
